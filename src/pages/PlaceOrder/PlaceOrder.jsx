f1f1b86229c2a2d454d93ccb498d2c5840d2b259
immadichaitanya <immadichaitanya@gmail.com>
2024-11-21 22:15:59 +0530

    last1

--- src/pages/PlaceOrder/PlaceOrder.jsx
+++ src/pages/PlaceOrder/PlaceOrder.jsx
@@ -9,7 +9,7 @@ const PlaceOrder = () => {
   const handleProceedToPayment = (event) => {
     event.preventDefault(); // Prevent default form submission
     alert('Ordered successfully');
-    window.location.reload();
+    window.location.assign('/order');
   };
 
   return (
